1cb1dd68f03984f656efe0fdbefa0fd236c1daf9
Yun.Lei <waitshan@163.com>
2020-01-15 14:40:49 +0800

    README

--- app/src/main/java/com/yunlei/hindicator/MainActivity.kt
+++ app/src/main/java/com/yunlei/hindicator/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.yunlei.hindicator
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -18,20 +19,26 @@ class MainActivity : AppCompatActivity() {
         hIndicator.bindRecyclerView(recyclerView)
 
 
+        btn1.setOnClickListener {
+            hIndicator.setBgColor(Color.parseColor("#333333"))
+        }
+        btn2.setOnClickListener {
+            hIndicator.setIndicatorColor(Color.parseColor("#ffffff"))
+        }
     }
 
     private inner class MAdapter : RecyclerView.Adapter<MViewHolder>() {
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MViewHolder {
-            return MViewHolder(
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MViewHolder =
+            MViewHolder(
                 LayoutInflater.from(parent.context).inflate(
                     R.layout.item_test,
                     parent,
                     false
                 )
             )
-        }
 
-        override fun getItemCount(): Int = 30
+
+        override fun getItemCount(): Int = 15
 
         override fun onBindViewHolder(holder: MViewHolder, position: Int) {
         }
